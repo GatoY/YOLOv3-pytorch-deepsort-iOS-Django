027eb47f41d7235b0d6c9863afe87fdd317b1b0c
WanYun <wanyuns@student.unimelb.edu.au>
2019-05-22 17:00:37 +1000

    another backend server with user authentication functions

---
+++ Wan-Yun app/backend/users/urls.py
@@ -0,0 +1,8 @@
+# users/urls.py
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+               path('', views.UserListView.as_view()),
+               ]
---
+++ Wan-Yun app/mvobj-iOS/mvobj/signupVC.swift
@@ -0,0 +1,84 @@
+//
+//  signupVC.swift
+//  mvobj
+//
+//  Created by WanYun Sun on 1/5/19.
+//  Copyright © 2019 WanYun Sun. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class signupVC: UIViewController {
+
+    
+    //textFields
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var rpPasswordTxt: UITextField!
+    
+    
+    //Buttons
+    @IBOutlet weak var SignupBtn: UIButton!
+    @IBOutlet weak var CancelBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+
+    @IBAction func signupBtnClick(_ sender: Any) {
+        print("signup button clicked")
+        //dismiss keyboard
+        self.view.endEditing(true)
+        
+        //check if all fields are filled
+        if emailTxt.text!.isEmpty || passwordTxt.text!.isEmpty || rpPasswordTxt.text!.isEmpty {
+            
+            //alert msg
+            let alert = UIAlertController(title: "Empty field(s)", message: "Please fill all fields.", preferredStyle: UIAlertController.Style.alert)
+            let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+        //check if repeat password correct
+        if passwordTxt.text != rpPasswordTxt.text {
+            
+            //alert msg
+            let alert = UIAlertController(title: "Passwords not match", message: "Repeat Password has to be the same as Password.", preferredStyle: UIAlertController.Style.alert)
+            let ok = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        //send signup info to server
+        
+
+        
+    }
+    
+    @IBAction func cancelBtnClick(_ sender: Any) {
+        print("cancel button clicked")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
