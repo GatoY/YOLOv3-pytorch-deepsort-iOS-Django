ce4a202404384555c1c1197991af2db3e8e786d7
WanYun <wanyuns@student.unimelb.edu.au>
2019-05-31 20:51:19 +1000

    user authentication updated

---
+++ Wan-Yun-app/backend/users/forms.py
@@ -0,0 +1,16 @@
+# users/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('email',)
+
+class CustomUserChangeForm(UserChangeForm):
+    
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
